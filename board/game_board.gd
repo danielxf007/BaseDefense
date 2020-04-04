f13b163cfc3ecf5c46ac0a43b6d2a466eb40362f
danielxf007 <daniel.higuita007@gmail.com>
2020-04-04 13:20:50 -0500

    game_board

--- board/game_board.gd
+++ board/game_board.gd
@@ -2,3 +2,91 @@ extends "res://board/board.gd"
 
 class_name GameBoard
 
+var selected_cells: Array = []
+
+func in_selected_cells(cell_pos: Tuple) -> int:
+	var cell: Cell
+	var cell_row: Array = self.selected_cells[cell_pos.j]
+	for i in range(0, cell_row.size()):
+		cell = cell_row[i]
+		if cell.board_coord.i == cell_pos.i:
+			return i
+	return -1
+
+func insert_selected_cell(pos: Tuple) -> void:
+	var cell: Cell
+	if selected_cells[pos.j].empty():
+		cell = self.matrix_of_cells[pos.i][pos.j]
+		cell.select_this_cell()
+		self.selected_cells[pos.j].append(cell)
+	else:
+		var cells_row: Array = self.selected_cells[pos.j]
+		var first_cell: Cell = cells_row[0]
+		var last_cell: Cell = cells_row[cells_row.size()-1]
+		var f_board_coord: Tuple = first_cell.board_coord
+		var l_board_cood: Tuple = last_cell.board_coord
+		if f_board_coord.i > pos.i:
+			cell = self.matrix_of_cells[pos.i][pos.j]
+			cell.select_this_cell()
+			self.selected_cells[pos.j].push_front(cell)
+		elif l_board_cood.i < pos.i:
+			cell = self.matrix_of_cells[pos.i][pos.j]
+			cell.select_this_cell()
+			self.selected_cells[pos.j].append(cell)
+		elif f_board_coord.i < pos.i and l_board_cood.i > pos.i:
+			var cell_to_insert: Cell = self.matrix_of_cells[pos.i][pos.j]
+			var first_part: Array = [first_cell]
+			var second_part: Array = [last_cell]
+			var index = cells_row.size()-1
+			for i in range(1, cells_row.size()-1):
+				cell = cells_row[index]
+				if cell.board_coord.i > cell_to_insert.board_coord.i:
+					index = i
+					break
+			first_part += cells_row.slice(1, index-1)
+			second_part += cells_row.slice(index,cells_row.size()-1)
+			cell_to_insert.select_this_cell()
+			cells_row = first_part + [cell_to_insert] + second_part
+			self.selected_cells[pos.j] = cells_row
+
+func eliminate_selected_cell(cell_pos: Tuple, pos_in: int) -> void:
+		var cell: Cell = self.matrix_of_cells[cell_pos.i][cell_pos.j]
+		cell.deselect_this_cell()
+		self.selected_cells[cell_pos.j].remove(pos_in)
+
+func _on_Player_player_selected_cell(pos: Tuple) -> void:
+	if not self.matrix_of_cells[pos.i][pos.j].is_occupied():
+		var pos_in: int = self.in_selected_cells(pos)
+		if pos_in == -1:
+			self.insert_selected_cell(pos)
+		else:
+			self.eliminate_selected_cell(pos, pos_in)
+
+func filter_selected_cells() -> void:
+	var filtered: Array = []
+	for element in self.selected_cells:
+		if not element.empty():
+			filtered.append(element)
+	self.selected_cells = filtered
+
+func reduce_selected_cells() -> void:
+	var accumulated: Array = []
+	for element in self.selected_cells:
+		accumulated += element
+	self.selected_cells = accumulated
+
+func deselect_cells() -> void:
+	for cell in self.selected_cells:
+		cell.deselect_this_cell()
+
+func belongs_to(group: Array, cell: Cell) -> bool:
+	var top_cell: Cell = group[group.size()-1]
+	return cell.board_coord.i - top_cell.board_coord.i == 1
+
+func _on_Player_pipe_line_placed():
+	self.filter_selected_cells()
+	self.reduce_selected_cells()
+	self.emit_signal("cells_selected",
+	self.selected_cells)
+	self.deselect_cells()
+	self.selected_cells.clear()
