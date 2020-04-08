921cc73ab905983321e1dbd9192e9d640820b2cb
danielxf007 <daniel.higuita007@gmail.com>
2020-04-08 13:16:22 -0500

    game_board

--- board/game_board.gd
+++ board/game_board.gd
@@ -8,3 +8,12 @@ func select_game_board_cell(cell_pos: Tuple) -> void:
 		self.array[cell_pos.i][cell_pos.j].select_cell()
 	else:
 		self.array[cell_pos.i][cell_pos.j].deselect_cell()
+
+
+func get_selected_cells() -> Array:
+	var selected_cells: Array = []
+	for i in range(0, self.dimensions.i):
+		for j in range(0, self.dimensions.j):
+			if self.array[i][j].is_selected():
+				selected_cells.append(self.array[i][j])
+	return selected_cells
